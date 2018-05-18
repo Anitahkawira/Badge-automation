a30ef4724caffbfb1bbe703067d40ed29c61414f
Millicent Achieng <millcient.achieng@twigafoods.com>
2018-05-18 18:13:56 +0300

    Add .gitignore file

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/anitah/84A0B4C4A0B4BDCC/Users/Anitah/Documents/badge-automation/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
